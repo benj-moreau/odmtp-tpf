90fc20d86e8bf07e315316a672790597c69b1347
benjimor <benji_moreau@hotmail.fr>
2017-11-06 11:22:04 +0100

    unauthenticated call to github api (limited to 60/Hour)

--- utils/github_api.py
+++ utils/github_api.py
@@ -2,8 +2,6 @@ import json
 
 from urllib2 import urlopen, Request, HTTPError
 
-TOKEN = '5f60b1f2d3bbd64536edb1a8d03ff0b8a536049a'
-
 
 class ClientException(Exception):
     pass
@@ -13,10 +11,8 @@ class GithubApi(object):
     """This class implements the Github api v3 authentication & request."""
 
     def request(self, url):
-        """Send an authenticated request to the Github API."""
+        """Send an unauthenticated request to the Github API."""
         request = Request(url)
-        request.add_header('Authorization',
-                           'token %s' % TOKEN)
         try:
             response = urlopen(request)
         except HTTPError:
