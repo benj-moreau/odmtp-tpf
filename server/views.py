40b508b7d208d1b02585118d418ee7cd10c7d065
amri-c <chanezamri@hotmail.fr>
2018-05-03 09:54:26 +0200

    added authentification for linkedin

--- server/views.py
+++ server/views.py
@@ -11,6 +11,12 @@ from tpf.fragment import Fragment
 
 from django.http.response import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
+
+from linkedin import linkedin
+from django.http import JsonResponse
+import requests
+from django.conf import settings
 
 
 @require_http_methods(['GET'])
@@ -63,10 +69,16 @@ def linkedin_tpf_server(request):
 
 @require_http_methods(['GET'])
 def linkedin_authentication_tpf_server(request):
-    fragment = Fragment()
-    response = HttpResponse(
-        fragment.serialize(),
-        content_type='application/trig; charset=utf-8')
-    response['Content-Disposition'] = 'attachment; filename="linkedin_auth.trig"'
-    response['Access-Control-Allow-Origin'] = '*'
+    authentication = linkedin.LinkedInAuthentication(settings.CLIENT_ID_LINKEDIN, settings.CLIENT_SECRET_LINKEDIN, "http://127.0.0.1:8000/linkedin/authentication/", ['r_emailaddress', 'r_basicprofile', 'w_share', 'rw_company_admin'])
+    authorization_code = request.GET.get('code', None)
+    if authorization_code:
+        authentication.authorization_code = authorization_code
+        linkedin_token = authentication.get_access_token()
+        response = HttpResponse(linkedin_token)
+        response['Access-Control-Allow-Origin'] = '*'
+    else:
+        application = linkedin.LinkedInApplication(authentication)
+        response = redirect(authentication.authorization_url)
+
+    # linkedin_token = authentication.get_access_token()
     return response
